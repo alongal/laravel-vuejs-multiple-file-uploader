c1863ce8a6211c8e5083a9810c12d266a54cb484
Alon Gal <alon@stampme.com>
2019-12-27 18:03:02 +1100

    uploaded file 1st approach

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
@@ -17,6 +18,27 @@ class ImageController extends Controller
             'file_name' => 'image|mimes:jpg,jpeg,png,gif,bmp',
             'title' => 'required'
         ]);
-        dd('ready to upload');
+
+        $image = new Image();
+        $image->title = $request->title;
+        $image->file_name = $this->uploadFile($request);
+        $image->save();
+
+        return redirect('/')->with('message', 'Your image successfully uploaded!');
+    }
+
+    protected function uploadFile($request)
+    {
+        if ($request->hasFile('file_name')) {
+            $image = $request->file('file_name');
+            $fileName = $image->getClientOriginalName();
+            $destimation = storage_path('app/public');
+
+            if ($image->move($destimation, $fileName)) {
+                return $fileName;
+            }
+        }
+
+        return null;
     }
 }
